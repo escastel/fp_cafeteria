6c5c11b308faae4c97a906ca312993f44bf2a6d2
Ester Castellano Ríos <estercastellanorios@gmail.com>
2026-01-20 15:19:33 +0100

    Prácticas con ViewModel

---
+++ app/src/main/java/com/example/cafeteria/ui/screen/AppUiState.kt
@@ -0,0 +1,15 @@
+package com.example.cafeteria.ui.screen
+
+import com.example.cafeteria.model.ItemUiModel
+
+/**
+ * Data class que contiene el estado de la pantalla principal.
+ */
+data class AppUiState(
+    val username: String = "",
+    val option: String = "Jamón",
+    val amount: Int = 0,
+    val orderList: List<ItemUiModel> = emptyList(),
+    val showDialog: Boolean = false,
+    val showErrorDialog: Boolean = false
+)
